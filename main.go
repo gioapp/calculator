8ed70924654660ebd230b3566205ca51606fd910
marcetin <marcetin@gmail.com>
2020-08-02 19:57:31 +0200

    initial

--- main.go
+++ main.go
@@ -7,56 +7,60 @@ import (
 	"gioui.org/font/gofont"
 	"gioui.org/io/system"
 	"gioui.org/layout"
-	"gioui.org/op/clip"
+	"gioui.org/op"
 	"gioui.org/op/paint"
+	"gioui.org/text"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 	"github.com/PaesslerAG/gval"
-	"github.com/p9c/learngio/helpers"
-	"image"
+	"github.com/w-ingsolutions/c/pkg/lyt"
 	"image/color"
 )
 
+type (
+	D = layout.Dimensions
+	C = layout.Context
+)
 type Calc struct {
-	Buttons     map[string]*widget.Button
+	Buttons     map[string]*widget.Clickable
 	Theme       *material.Theme
 	Calculation string
 }
 
 func NewCalc() Calc {
-	buttons := map[string]*widget.Button{
-		"0": new(widget.Button),
-		"1": new(widget.Button),
-		"2": new(widget.Button),
-		"3": new(widget.Button),
-		"4": new(widget.Button),
-		"5": new(widget.Button),
-		"6": new(widget.Button),
-		"7": new(widget.Button),
-		"8": new(widget.Button),
-		"9": new(widget.Button),
-		"+": new(widget.Button),
-		"-": new(widget.Button),
-		"/": new(widget.Button),
-		"*": new(widget.Button),
-		".": new(widget.Button),
-		"=": new(widget.Button),
-		"c": new(widget.Button),
-		")": new(widget.Button),
-		"(": new(widget.Button),
-		"←": new(widget.Button),
+	buttons := map[string]*widget.Clickable{
+		"0": new(widget.Clickable),
+		"1": new(widget.Clickable),
+		"2": new(widget.Clickable),
+		"3": new(widget.Clickable),
+		"4": new(widget.Clickable),
+		"5": new(widget.Clickable),
+		"6": new(widget.Clickable),
+		"7": new(widget.Clickable),
+		"8": new(widget.Clickable),
+		"9": new(widget.Clickable),
+		"+": new(widget.Clickable),
+		"-": new(widget.Clickable),
+		"/": new(widget.Clickable),
+		"*": new(widget.Clickable),
+		".": new(widget.Clickable),
+		"=": new(widget.Clickable),
+		"c": new(widget.Clickable),
+		")": new(widget.Clickable),
+		"(": new(widget.Clickable),
+		"←": new(widget.Clickable),
 	}
 	return Calc{
 		Buttons: buttons,
-		Theme:   material.NewTheme(),
+		Theme:   material.NewTheme(gofont.Collection()),
 	}
 }
 
-func (c *Calc) Button(gtx *layout.Context, b string) func() {
-	return func() {
-		layout.UniformInset(unit.Dp(16)).Layout(gtx, func() {
-			for c.Buttons[b].Clicked(gtx) {
+func (c *Calc) Button(b string) func(gtx C) D {
+	return func(gtx C) D {
+		return layout.UniformInset(unit.Dp(16)).Layout(gtx, func(gtx C) D {
+			for c.Buttons[b].Clicked() {
 				switch b {
 				case "=":
 					cc, _ := gval.Evaluate(c.Calculation, nil)
@@ -71,99 +75,96 @@ func (c *Calc) Button(gtx *layout.Context, b string) func() {
 					c.Calculation = c.Calculation + b
 				}
 			}
-			btn := c.Theme.Button(b)
-			btn.Background = helpers.HexARGB("ff888888")
+			btn := material.Button(c.Theme, c.Buttons[b], b)
+			btn.Background = HexARGB("ff888888")
 			switch b {
 			case "=":
-				btn.Background = helpers.HexARGB("ff30cf30")
+				btn.Background = HexARGB("ff30cf30")
 			case "c":
-				btn.Background = helpers.HexARGB("ffcf3030")
+				btn.Background = HexARGB("ffcf3030")
 			case "←":
-				btn.Background = helpers.HexARGB("ffcfcf30")
+				btn.Background = HexARGB("ffcfcf30")
 			case "(", ")":
-				btn.Background = helpers.HexARGB("ffcf30cf")
+				btn.Background = HexARGB("ffcf30cf")
 			case "/", "*", "-", "+":
-				btn.Background = helpers.HexARGB("ff3030cf")
+				btn.Background = HexARGB("ff3030cf")
 			default:
-				btn.Background = helpers.HexARGB("ff30cfcf")
+				btn.Background = HexARGB("ff30cfcf")
 			}
-			btn.Layout(gtx, c.Buttons[b])
+			return btn.Layout(gtx)
 		})
 	}
 }
 
 func main() {
-	gofont.Register()
 	calcApp := NewCalc()
+
 	go func() {
 		w := app.NewWindow(
 			app.Size(unit.Dp(500), unit.Dp(800)),
 			app.Title("ParallelCoin"),
 		)
-		gtx := layout.NewContext(w.Queue())
-		for e := range w.Events() {
-			if e, ok := e.(system.FrameEvent); ok {
-				gtx.Reset(e.Config, e.Size)
-				flex(gtx, layout.Vertical,
-					flexChild(gtx, 0.2, "ffcfcfcf", func() {
-						layout.E.Layout(gtx,
-							func() {
-								calcApp.Theme.H3(calcApp.Calculation).Layout(gtx)
+		var ops op.Ops
+		for {
+			select {
+			case e := <-w.Events():
+				switch e := e.(type) {
+				case system.FrameEvent:
+					gtx := layout.NewContext(&ops, e)
+					Fill(gtx, HexARGB("ff303030"))
+					lyt.Format(gtx, "vflexa(middle,f(0.2,_),f(0.8,_))",
+						func(gtx C) D {
+							return layout.UniformInset(unit.Dp(16)).Layout(gtx, func(gtx C) D {
+								Fill(gtx, HexARGB("ffffffff"))
+								return layout.UniformInset(unit.Dp(16)).Layout(gtx, func(gtx C) D {
+									out := material.H3(calcApp.Theme, calcApp.Calculation)
+									out.Alignment = text.End
+									return out.Layout(gtx)
+								})
 							})
-					}),
-					flexChild(gtx, 0.8, "ff303030", flex(gtx, layout.Horizontal,
-						calcApp.column(gtx, "(", "7", "4", "1", "0"),
-						calcApp.column(gtx, ")", "8", "5", "2", "."),
-						calcApp.column(gtx, "c", "9", "6", "3", "="),
-						calcApp.column(gtx, "←", "/", "*", "-", "+"),
-					)),
-				)()
-				e.Frame(gtx.Ops)
+						},
+						func(gtx C) D {
+							return lyt.Format(gtx, "hflexa(middle,f(0.2,_),f(0.2,_),f(0.2,_),f(0.2,_))",
+								calcApp.column("(", "7", "4", "1", "0"),
+								calcApp.column(")", "8", "5", "2", "."),
+								calcApp.column("c", "9", "6", "3", "+"),
+								calcApp.column("←", "/", "*", "-", "="),
+							)
+						},
+					)
+					e.Frame(gtx.Ops)
+				}
+				w.Invalidate()
 			}
 		}
 	}()
 	app.Main()
 }
 
-func (c *Calc) column(gtx *layout.Context, ba, bb, bc, bd, be string) layout.FlexChild {
-	return flexChild(gtx, 0.25, "ff303030", flex(gtx, layout.Vertical,
-		flexChild(gtx, 0.2, "ff303030", c.Button(gtx, ba)),
-		flexChild(gtx, 0.2, "ff303030", c.Button(gtx, bb)),
-		flexChild(gtx, 0.2, "ff303030", c.Button(gtx, bc)),
-		flexChild(gtx, 0.2, "ff303030", c.Button(gtx, bd)),
-		flexChild(gtx, 0.2, "ff303030", c.Button(gtx, be)),
-	))
-}
-
-func flex(gtx *layout.Context, axis layout.Axis, content ...layout.FlexChild) func() {
-	return func() {
-		layout.Flex{
-			Axis: axis,
-		}.Layout(gtx, content...)
+func (c *Calc) column(ba, bb, bc, bd, be string) func(gtx C) D {
+	return func(gtx C) D {
+		return lyt.Format(gtx, "vflexa(middle,f(0.2,_),f(0.2,_),f(0.2,_),f(0.2,_),f(0.2,_))",
+			c.Button(ba),
+			c.Button(bb),
+			c.Button(bc),
+			c.Button(bd),
+			c.Button(be),
+		)
 	}
 }
 
-func flexChild(gtx *layout.Context, size float32, bg string, content func()) layout.FlexChild {
-	return layout.Flexed(size, func() {
-		cs := gtx.Constraints
-		drawRectangle(gtx, cs.Width.Max, cs.Height.Max, helpers.HexARGB(bg), [4]float32{0, 0, 0, 0}, unit.Dp(0))
-		content()
-	})
+func HexARGB(s string) (c color.RGBA) {
+	_, _ = fmt.Sscanf(s, "%02x%02x%02x%02x", &c.A, &c.R, &c.G, &c.B)
+	return
 }
 
-func drawRectangle(gtx *layout.Context, w, h int, color color.RGBA, borderRadius [4]float32, inset unit.Value) {
-	in := layout.UniformInset(inset)
-	in.Layout(gtx, func() {
-		square := f32.Rectangle{
-			Max: f32.Point{
-				X: float32(w),
-				Y: float32(h),
-			},
-		}
-		paint.ColorOp{Color: color}.Add(gtx.Ops)
-		clip.Rect{Rect: square,
-			NE: borderRadius[0], NW: borderRadius[1], SE: borderRadius[2], SW: borderRadius[3]}.Op(gtx.Ops).Add(gtx.Ops) // HLdraw
-		paint.PaintOp{Rect: square}.Add(gtx.Ops)
-		gtx.Dimensions = layout.Dimensions{Size: image.Point{X: w, Y: h}}
-	})
+func Fill(gtx layout.Context, col color.RGBA) layout.Dimensions {
+	cs := gtx.Constraints
+	d := cs.Min
+	dr := f32.Rectangle{
+		Max: f32.Point{X: float32(d.X), Y: float32(d.Y)},
+	}
+	paint.ColorOp{Color: col}.Add(gtx.Ops)
+	paint.PaintOp{Rect: dr}.Add(gtx.Ops)
+	return layout.Dimensions{Size: d}
 }
